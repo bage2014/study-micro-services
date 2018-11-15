6b7762e013084eb18183954a9354d51207057d0e
bage2014 <893542907@qq.com>
2018-11-15 17:01:29 +0800

    增加actuator

---
+++ study-micro-services-spring-boot/src/main/java/com/bage/study/micro/services/spring/boot/aop/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.bage.study.micro.services.spring.boot.aop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class UserController {
+
+	public String get() {
+		return "";
+	}
+	
+}
