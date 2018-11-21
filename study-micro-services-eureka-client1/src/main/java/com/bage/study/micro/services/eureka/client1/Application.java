2aef80973e95a2ca7d921fd1fc6005f4b9b916a1
bage2014 <893542907@qq.com>
2018-11-21 16:20:49 +0800

    新增eureka server

--- study-micro-services-eureka-client1/src/main/java/com/bage/study/micro/services/eureka/client1/Application.java
+++ study-micro-services-eureka-client1/src/main/java/com/bage/study/micro/services/eureka/client1/Application.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableDiscoveryClient
+@RestController
 public class Application {
 
     @RequestMapping("/")
