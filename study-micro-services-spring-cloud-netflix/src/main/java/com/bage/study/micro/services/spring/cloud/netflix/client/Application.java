7f2fc1bca88f4318897dfb4f43db0ed05def41d6
bage2014 <893542907@qq.com>
2018-11-21 10:30:24 +0800

    新增Netflix

---
+++ study-micro-services-spring-cloud-netflix/src/main/java/com/bage/study/micro/services/spring/cloud/netflix/client/Application.java
@@ -0,0 +1,21 @@
+package com.bage.study.micro.services.spring.cloud.netflix.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+    	SpringApplication.run(Application.class, args);
+    }
+
+}
