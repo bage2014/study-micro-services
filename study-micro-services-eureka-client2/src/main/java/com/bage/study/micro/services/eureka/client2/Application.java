73f66591335f219f66a62944c3daa50bd0004ea6
bage2014 <893542907@qq.com>
2018-11-21 18:54:55 +0800

    新增eureka client2

---
+++ study-micro-services-eureka-client2/src/main/java/com/bage/study/micro/services/eureka/client2/Application.java
@@ -0,0 +1,23 @@
+package com.bage.study.micro.services.eureka.client2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+    	SpringApplication.run(Application.class, args);
+    }
+
+}
