0edc8910db49f3dc5bea26c8364b00fcb28b7bb4
bage2014 <893542907@qq.com>
2018-11-22 08:34:00 +0800

    获取所有的注册服务

--- study-micro-services-eureka-client2/src/main/java/com/bage/study/micro/services/eureka/client2/EurekaClientController.java
+++ study-micro-services-eureka-client2/src/main/java/com/bage/study/micro/services/eureka/client2/EurekaClientController.java
@@ -1,23 +1,29 @@
 package com.bage.study.micro.services.eureka.client2;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Applications;
 
 @RestController
 @RequestMapping("eurekaClient")
 public class EurekaClientController {
 	
 	@Autowired
-	private EurekaClient discoveryClient;
+	private DiscoveryClient discoveryClient;
 	
-	@RequestMapping("serviceUrl")
-	public String serviceUrl() {
-	    Applications apps = discoveryClient.getApplications();
-	    return apps.getAppsHashCode();
+	@GetMapping("/clients")
+	public String client() {
+		List<String> list = discoveryClient.getServices();
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < list.size(); i++) {
+			stringBuilder.append(list.get(i) + ",");
+		}
+		System.out.println(stringBuilder.toString());
+		return stringBuilder.toString();
 	}
 }
 
--- study-micro-services-eureka/src/main/java/com/bage/study/micro/services/eureka/EurekaClientController.java
+++ study-micro-services-eureka/src/main/java/com/bage/study/micro/services/eureka/EurekaClientController.java
@@ -3,29 +3,27 @@ package com.bage.study.micro.services.eureka;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Application;
-import com.netflix.discovery.shared.Applications;
 
 @RestController
 @RequestMapping("eurekaClient")
 public class EurekaClientController {
-	
+
 	@Autowired
-	private EurekaClient discoveryClient;
-	
-	@RequestMapping("serviceUrl")
-	public String serviceUrl() {
-		Applications apps = discoveryClient.getApplications();
-		List<Application> list = apps.getRegisteredApplications();
+	private DiscoveryClient discoveryClient;
+
+	@GetMapping("/clients")
+	public String client() {
+		List<String> list = discoveryClient.getServices();
 		StringBuilder stringBuilder = new StringBuilder();
 		for (int i = 0; i < list.size(); i++) {
-			stringBuilder.append(list.get(i).getName() + ",");
+			stringBuilder.append(list.get(i) + ",");
 		}
+		System.out.println(stringBuilder.toString());
 		return stringBuilder.toString();
 	}
-}
 
+}
