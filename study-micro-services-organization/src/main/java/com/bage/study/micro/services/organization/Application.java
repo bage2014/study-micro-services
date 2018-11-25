5d1845454ccedea896c747b3f9828ca71620b1fd
bage2014 <893542907@qq.com>
2018-11-25 14:50:32 +0800

    修改编译版本为 1.8

--- study-micro-services-organization/src/main/java/com/bage/study/micro/services/organization/Application.java
+++ study-micro-services-organization/src/main/java/com/bage/study/micro/services/organization/Application.java
@@ -9,12 +9,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-@SpringBootApplication
+@EnableAutoConfiguration
 @ComponentScan(basePackageClasses=Application.class)
 @EnableCaching
 public class Application implements CommandLineRunner {
