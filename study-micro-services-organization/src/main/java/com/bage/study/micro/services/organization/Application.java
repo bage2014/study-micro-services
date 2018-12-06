d8b0dfababfc4f79d60ed501320ce3919897863e
bage2014 <893542907@qq.com>
2018-12-06 20:52:06 +0800

    增加组织机构demo

--- study-micro-services-organization/src/main/java/com/bage/study/micro/services/organization/Application.java
+++ study-micro-services-organization/src/main/java/com/bage/study/micro/services/organization/Application.java
@@ -10,14 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackageClasses=Application.class)
 @EnableCaching
+@EnableDiscoveryClient
 public class Application implements CommandLineRunner {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
