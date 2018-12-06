d36c99f8412b5cb439ed595be090a3d8bcd7edb3
bage2014 <893542907@qq.com>
2018-12-06 17:56:17 +0800

    增加网关服务

--- study-micro-services-zuul/src/main/java/com/bage/study/micro/services/ribbon/ZuulApplication.java
+++ study-micro-services-zuul/src/main/java/com/bage/study/micro/services/ribbon/ZuulApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableZuulProxy
 public class ZuulApplication {
 
+	@Bean
+	public PreRequestLogFilter preRequestLogFilter() {
+		return new PreRequestLogFilter();
+	}
+	
     @RequestMapping("/")
     public String home() {
         return "Hello world";
