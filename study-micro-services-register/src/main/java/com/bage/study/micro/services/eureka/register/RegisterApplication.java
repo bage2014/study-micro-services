463fdbc0d5fae550c683b2c38c86b156eaf40fc8
bage2014 <893542907@qq.com>
2018-12-17 13:09:32 +0800

    增加注册中心服务[未完成]

---
+++ study-micro-services-register/src/main/java/com/bage/study/micro/services/eureka/register/RegisterApplication.java
@@ -0,0 +1,26 @@
+package com.bage.study.micro.services.eureka.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class RegisterApplication {
+
+	@RequestMapping("/")
+    public String home(HttpServletRequest request) {
+    	System.out.println("这是client register ");
+        return "Hello register " + request.getContextPath();
+    }
+
+    public static void main(String[] args) {
+    	SpringApplication.run(RegisterApplication.class, args);
+    }
+
+}
