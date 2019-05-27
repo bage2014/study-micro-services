997154feee961b7f04725624aac1a64b6e685129
bage2014 <893542907@qq.com>
2019-05-27 23:54:22 +0800

    配置中心内置对象的api

--- study-micro-services-config-client/src/main/java/com/bage/study/micro/services/eureka/register/MessageRestController.java
+++ study-micro-services-config-client/src/main/java/com/bage/study/micro/services/eureka/register/MessageRestController.java
@@ -1,7 +1,10 @@
 package com.bage.study.micro.services.eureka.register;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.refresh.ContextRefresher;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageRestController {
 
-    @Value("${name:Hello default}")
+    @Value("${Header.title:Hello default}")
     private String name;
     @Value("${message:Hello default}")
     private String message;
+
+    @Autowired
+    private ConfigurableEnvironment environment;
+
+    @Autowired
+    private ContextRefresher refresher;
+
+    @RequestMapping("/environment")
+    String environment() {
+        return environment.toString();
+    }
+
+    @RequestMapping("/refresher")
+    String refresher() {
+        return String.valueOf(refresher.refresh());
+    }
 
     @RequestMapping("/name")
     String getConfig() {
--- study-micro-services-config/README.md
+++ study-micro-services-config/README.md
@@ -3,5 +3,5 @@
 
 ## 参考链接 ##
 - Centralized Configuration [https://spring.io/guides/gs/centralized-configuration/](https://spring.io/guides/gs/centralized-configuration/)
-
+- Spring Cloud Config [https://cloud.spring.io/spring-cloud-config/spring-cloud-config.html](https://cloud.spring.io/spring-cloud-config/spring-cloud-config.html)
 
